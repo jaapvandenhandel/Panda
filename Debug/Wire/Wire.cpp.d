553531306b151533e938f8c972eff6e1f459ff99
Brian Tischler <yesgogit@gmail.com>
2022-01-14 09:00:52 -0700

    delete folders

--- Debug/Wire/Wire.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Grizs\AppData\Local\Temp\VMBuilds\Panda\teensy41\Debug\Wire\Wire.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire\Wire.cpp
--- Release/Wire/utility/twi.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Grizs\AppData\Local\Temp\VMBuilds\Panda\teensy41\Release\Wire\utility\twi.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire\utility\twi.c
